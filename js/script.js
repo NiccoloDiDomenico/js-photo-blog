01f9e8b4fbb64e3994eaf78e796f0470dc469e83
NiccoloDiDomenico <niccolodidomenico99@gmail.com>
2024-11-19 16:47:20 +0100

    fix: fix JS code

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@
 const photoBlogGridElements = document.getElementById("photo-blog-grid")
 console.log(photoBlogGridElements);
 
+// Functions
+
 
 // Logic
 
@@ -10,18 +12,20 @@ axios.get(`https://jsonplaceholder.typicode.com/photos?_limit=6`)
     .then((response) => {
         console.log(response.data);
         
+        // Creo ciclo forEach con l'array di oggetti presi dalla chiamata
         response.data.forEach(curElement => {
+            // Creo due variabili gli assegno i valori delle chiavi degli oggetti presi dalla chiamata
             let url = curElement.url;
             let titles = curElement.title;
             console.log(url, titles);
 
+            // Inserisco le varibili nel codice e lo rendo dinamico
             photoBlogGridElements.innerHTML += `
                 <div class="col">
                     <div class="card">
                         <img src="${url}" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">${titles}</h5>
-                            <p class="card-text">Card body</p>
                         </div>
                     </div>
                 </div>
